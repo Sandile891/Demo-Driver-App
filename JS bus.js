ffd2ffd4096527a22bf1b13a6bf89ffc23a005e4
Sandile891 <mnisisandile16@gmail.com>
2024-10-07 20:57:30 +0200

    Update JS bus.js

--- JS bus.js
+++ JS bus.js
@@ -47,4 +47,15 @@ self.addEventListener('push', function(event) {
   );
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(err) {
+        console.log('Service Worker registration failed:', err);
+      });
+  });
+}
+
   
