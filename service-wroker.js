c162deb5c3781f5bca704561967d668ee0a7b488
Sandile891 <mnisisandile16@gmail.com>
2024-10-08 12:02:37 +0200

    Update service-wroker.js

--- service-wroker.js
+++ service-wroker.js
@@ -1,18 +1,13 @@
-// service-worker.js
-
-const CACHE_NAME = 'putco-bus-cache-v1';
+const CACHE_NAME = 'putco-bus-app-cache-v1';
 const urlsToCache = [
   '/',
-  '/Putco-Bus-App/',
-  '/Putco-Bus-App/index.html',  // Replace with your actual HTML file paths
-  '/Putco-Bus-App/Buying trips.html', 
-  '/Putco-Bus-App/Live tracking.html',
-  '/Putco-Bus-App/ic.png',  // Add paths to your images and other assets
-  '/Putco-Bus-App/your-image-192.png'
+  '/index.html',
+  '/assets/ic.png',
+  '/assets/CSS Bus.css',
+  '/assets/JS bus.js',
 ];
-
-// Install a service worker
 self.addEventListener('install', event => {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -21,18 +16,18 @@ self.addEventListener('install', event => {
       })
   );
 });
-
-// Cache and return requests
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
   );
 });
-
-// Update the service worker
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -47,5 +42,4 @@ self.addEventListener('activate', event => {
     })
   );
 });
-
 
