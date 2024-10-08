c313f56d8cf99e85d4c309c8ae05a8820d76282d
Sandile891 <mnisisandile16@gmail.com>
2024-10-08 12:17:27 +0200

    Update service-wroker.js

--- service-wroker.js
+++ service-wroker.js
@@ -42,4 +42,29 @@ self.addEventListener('activate', event => {
     })
   );
 });
+[[[[[[[
+
+  self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/Live tracking.html',
+        '/Buying trips.html',
+        '/ic.png',
+        '/ic.png',
+        // Add other assets you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 
