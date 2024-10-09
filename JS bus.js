0e0743e4ce8444356ebdecaeda0d38dc3ead5310
Sandile891 <mnisisandile16@gmail.com>
2024-10-09 14:15:43 +0200

    Update JS bus.js

--- JS bus.js
+++ JS bus.js
@@ -86,3 +86,13 @@ if ('serviceWorker' in navigator) {
         });
 }
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/Putco-Bus-App/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function(error) {
+            console.error('Service Worker registration failed:', error);
+        });
+}
+
