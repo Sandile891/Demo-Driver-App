80f19396130277fd2da95e3723d5893bf21bb3f4
Sandile891 <mnisisandile16@gmail.com>
2024-10-09 21:47:57 +0200

    Update JS bus.js

--- JS bus.js
+++ JS bus.js
@@ -64,4 +64,15 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 //////
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then((registration) => {
+              console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+              console.error('Service Worker registration failed:', error);
+          });
+  });
+}
 
