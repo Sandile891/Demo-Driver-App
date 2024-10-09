bffaee1176019a32a30c7952517abeb3bc62e730
Sandile891 <mnisisandile16@gmail.com>
2024-10-09 10:12:25 +0200

    Update JS bus.js

--- JS bus.js
+++ JS bus.js
@@ -101,3 +101,25 @@ navigator.serviceWorker.ready.then(function(registration) {
     console.log('Failed to subscribe user: ', err);
   });
 });
+
+------
+
+    const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
