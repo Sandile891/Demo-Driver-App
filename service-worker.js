b58f206565be5fe0f9ff4ba8d1c59feb8f0c49ee
Sandile891 <mnisisandile16@gmail.com>
2024-10-09 17:42:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,8 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/CSS Bus.css', // Add your CSS files here
-  '/JS bus.js',  // Add your JavaScript files here
-  // Add other assets like images, etc.
-];
-
-self.addEventListener('install', function(event) {
-  // Perform the install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activated');
 });
 
-self.addEventListener('activate', function(event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
