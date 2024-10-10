da801b93aef89ecc5fb17ebeea239d19989cb691
Sandile891 <mnisisandile16@gmail.com>
2024-10-10 10:47:40 +0200

    Update JS bus.js

--- JS bus.js
+++ JS bus.js
@@ -62,17 +62,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
   });
 });
 
-//////
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/Putco-Bus-App/service-worker.js')
-          .then((registration) => {
-              console.log('Service Worker registered with scope:', registration.scope);
-          })
-          .catch((error) => {
-              console.error('Service Worker registration failed:', error);
-          });
-  });
-}
 
