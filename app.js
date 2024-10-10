575d3c04710a02158c61f0ad0dd881924334fa10
Sandile891 <mnisisandile16@gmail.com>
2024-10-10 11:43:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ if ('serviceWorker' in navigator) {
       .then((reg) => console.log('Service worker registered', reg))
       .catch((err) => console.log('Service worker not registered', err));
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then((registration) => {
+              console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+              console.error('Service Worker registration failed:', error);
+          });
+  });
+}
